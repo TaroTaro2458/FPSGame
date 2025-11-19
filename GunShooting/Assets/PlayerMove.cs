044e776f914ed7ccda22001896823e84fb2d3c18
STUDENT\2311118 <yutailangp@gmail.com>
2025-11-19 12:28:45 +0900

    ステージ（仮）作成中　ダッシュ機能の追加（左シフト長押し）

--- GunShooting/Assets/PlayerMove.cs
+++ GunShooting/Assets/PlayerMove.cs
@@ -9,10 +9,23 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundMask;
 
+    [SerializeField] float maxStamina = 100f;// 最大スタミナ
+    private float currentStamina;  // 現在のスタミナ
+    [SerializeField] float staminaDrainRate = 20f; // 毎秒減る量
+    [SerializeField] float staminaRecoveryRate = 10f; // 毎秒回復する量
+
     //ジャンプの力
     [SerializeField] float jumpForce = 5f;
-
+    // 地面に接地しているかどうか
     [SerializeField] bool isGrounded;
+    // ダッシュの速度と持続時間
+    [SerializeField] float dashSpeed = 10f;
+    [SerializeField] float dashDuration = 5f;
+
+    private float dashTimer ;
+    private bool isDashing = false;
+    private float dashEndTime = 0f;
+
 
 
     void Start()
@@ -31,16 +44,34 @@ public class PlayerMove : MonoBehaviour
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+        bool isTryingToDash = Input.GetKey(KeyCode.LeftShift);
+        bool canDash = currentStamina > 0f;
+
+        if (isTryingToDash && canDash)
+        {
+            currentStamina -= staminaDrainRate * Time.deltaTime;
+            currentStamina = Mathf.Max(currentStamina, 0f);
+        }
+        else
+        {
+            currentStamina += staminaRecoveryRate * Time.deltaTime;
+            currentStamina = Mathf.Min(currentStamina, maxStamina);
+        }
     }
 
     void FixedUpdate()
     {
-        //前後左右の移動
+        // 移動入力
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+
+        float currentSpeed = (Input.GetKey(KeyCode.LeftShift) && currentStamina > 0f) ? dashSpeed : moveSpeed;
+        
+        rb.MovePosition(rb.position + move * currentSpeed * Time.fixedDeltaTime);
+
     }
 }
 
